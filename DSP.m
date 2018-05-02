e9b2db42586a3e031748f455e6527db5c722a43a
stamaleiferis <saleiferis@gmail.com>
2018-05-01 21:46:58 -0400

    Add files via upload

--- DSP.m
+++ DSP.m
@@ -1,12 +1,25 @@
-%import java.awt.Robot;
-%import java.awt.event.*;
-%robot = java.awt.Robot;
-%sport = '/dev/cu.usbmodem1D1142';
-%sport = '/dev/cu.usbmodem1412';
-sport = 'COM5';
+import java.awt.Robot;
+import java.awt.event.*;
+global mouse; 
+global ax_vec;
+global ax_sum;
+global ay_vec;
+global n;
+global cutoff;
+global factor; %takes into account dt and mouse move factor
+
+mouse = java.awt.Robot;
+ax_vec = [];
+ax_sum = 0;
+ay_vec = 0;
+n = 1;
+cutoff = 100;
+factor = 1;
+
+sport = 'COM3';
 s = serial(sport);  
 s.BytesAvailableFcnMode = 'terminator';
-
+s.InputBufferSize = 24000;
 %s.BytesAvailableFcnCount = 6;
 set(s,'BaudRate',1000000);    %set Baud rate
 s.BytesAvailableFcn = {@getValues}; %callback function when new set of data arrives
--- getValues.m
+++ getValues.m
@@ -1,16 +1,38 @@
 function [ax, ay, az] = getValues(obj, ~)
-    %fprintf(fgetl(obj));
-    %fprintf('inside');
-    persistent n;
-    if isempty(n) 
-        n=0;
-    end
+    %% import and initialize variables
+    global mouse; 
+    global ax_vec;
+    global ay_vec;
+    global n;
+    global cutoff
+    global ax_sum;
+    %% read values from serial
     tline = fgetl(obj);
     vals = str2double(strsplit(tline, '|'));
-    ax = vals(1);
-    ay = vals(2);
+    %%
+    ax = vals(1);   % get ax value
+    %ax_vec(n) = ax; % add ax value to array
+    ay = vals(2);   % get ay value
     az = vals(3);
-    fprintf('%d | ax: %0.5f \t ay: %0.5f \t az: %0.5f\n', n, ax,ay,az);
-    stem(n,ax);
+    ax_sum = ax_sum + ax;
+    %fprintf('%d | ax: %0.5f \t ay: %0.5f \t az: %0.5f\n', n, ax,ay,az);
+    %stem(n,ax);
     n = n+1;
+    
+    %% if ax and ay vectors are full, calculate displacement
+%     if length(ax_vec) == cutoff
+%        %calculate displacement
+%        d = sum(ax_vec);
+%        fprintf('!!!%d\n', d);
+%        ax_vec = [];
+%        fprintf('Cleared array, curr length is: %d', length(ax_vec));
+%        %move the mouse
+%     end
+    %% if n acceleration values have been read, move the mouse
+    if n == cutoff
+        d = ax_sum * 1;
+        fprintf('d is: %d\n',d);
+        ax_sum = 0;
+        n = 1;
+    end
 end
